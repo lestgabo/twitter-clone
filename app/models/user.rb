974a369b246246906968258c27ea77595e386582
lestgabo <lestgabo@gmail.com>
2018-07-05 11:05:21 +0000

    Add user following - COMPLETED TWITTER SAMPLE APP

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed  
+  has_many :followers, through: :passive_relationships, source: :follower
+  
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -74,10 +83,30 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
   
-  # defines a proto-feed
-  # see "Following users" for the full implementation
+  # returns a user's status feed
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                               WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) 
+                     OR user_id = :user_id",
+                     user_id: self.id)
+  end
+  
+  # follows a user
+  def follow(other_user)
+    # active_relationships.create(followed_id: other_user.id)
+    self.following << other_user
+  end
+  
+  # unfollow a user
+  def unfollow(other_user)
+    # active_relationships.find_by(followed_id: other_user.id).destroy
+    self.following.delete(other_user)
+  end
+  
+  # returns true if the current user is following the other user
+  def following?(other_user)
+    self.following.include?(other_user)
   end
   
   private
